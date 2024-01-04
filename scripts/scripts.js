e76622827c50e82d4d97008f81111b99c2afbb9b
unknown <windieciprof@gmail.com>
2024-01-04 15:24:24 +0100

    Fixed script.js

--- scripts/scripts.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector('.fade-in-title').style.opacity = 1;
-    document.querySelector('.fade-in-text').style.opacity = 1;
-});
